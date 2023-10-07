300bd0e0dd2e0a1b4386f2a468b02bae13590726
= <=>
2023-10-07 22:33:38 +0100

    feat: main routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./pages/home/home-routing.module').then(m => m.HomeRoutingModule) },
+  { path: 'auth', loadChildren: () => import('./pages/authentication/authentication-routing.module').then(m => m.AuthenticationRoutingModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
